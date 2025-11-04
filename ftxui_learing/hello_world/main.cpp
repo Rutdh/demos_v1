fef7b92b08ee20ee7f0fb152f64d9f0568bafdba
ganjun <1320957532@qq.com>
2025-11-04 23:54:46 +0800

    写了一些示例代码

--- ftxui_learing/hello_world/main.cpp
+++ ftxui_learing/hello_world/main.cpp
@@ -1,7 +1,33 @@
+#include "ftxui/screen/color.hpp"
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <iostream>
- 
+
+void PrintX() {
+    auto screen = ftxui::Screen::Create(
+    ftxui::Dimension::Full(),
+    ftxui::Dimension::Fixed(10)
+  );
+
+  auto &pixel = screen.PixelAt(10, 5);
+
+  pixel.character = U'X';
+  pixel.foreground_color = ftxui::Color::Red;
+  pixel.background_color = ftxui::Color::RGB(0, 255, 0);
+  pixel.bold = true;
+  screen.Print();
+
+  std::cout << "test ftxui::Screen::ToString(): " 
+    << screen.ToString() << std::endl;
+}
+
+void PrintCurTerminalDefaultColor() {
+  auto var = ftxui::Color::Default;
+  // std::cout << "current terminal default color is: "
+  //   << var << std::endl;
+  
+}
+
 int main() {
   using namespace ftxui;
  
@@ -14,4 +40,14 @@ int main() {
   auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
   Render(screen, document);
   screen.Print();
+  std::cout << "test ftxui::Screen::ToString(): " 
+    << screen.ToString() << std::endl;
+
+  screen.Clear();
+
+  PrintX();
+
+  screen.Clear();
+
+  PrintCurTerminalDefaultColor();
 }
